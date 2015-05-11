b2cfa3c8c1d40485c05ca5201af2b905af1df7dc
Loring Dodge <loringdodge@yahoo.com>
2015-05-11 11:15:26 -0700

    Add skeleton for RequestFactory

--- www/js/request/RequestFactory.js
+++ www/js/request/RequestFactory.js
@@ -2,5 +2,35 @@ angular.module('app.RequestFactory', [])
 
   .factory('RequestFactory', function($http){
 
+    /*
+      VARIABLES
+    */
+
+      var thing = {};
+
+    /*
+      HELPER FUNCTIONS
+    */
+
+      var something = function() {
+
+      }
+
+    /*
+      HTTP FUNCTIONS
+    */
+
+      // Post a new request
+      var postRequest = function() {
+
+      }
+
+    /*
+      RETURN
+    */
+    return {
+      postRequest: postRequest
+    }
+
 
   });
