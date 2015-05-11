2f5df09cad5d0a1e53dac22fe9349d903da44995
Loring Dodge <loringdodge@yahoo.com>
2015-05-11 10:49:42 -0700

    Add skeleton for AgentFactory

--- www/js/agent/AgentFactory.js
+++ www/js/agent/AgentFactory.js
@@ -2,5 +2,40 @@ angular.module('app.AgentFactory', [])
 
   .factory('AgentFactory', function($http){
 
+    /*
+      VARIABLES
+    */
+
+      var thing = {};
+
+    /*
+      HELPER FUNCTIONS
+    */
+
+      var something = function() {
+
+      }
+
+    /*
+      HTTP FUNCTIONS
+    */
+
+      // Login user
+      var getDirectory = function() {
+
+      }
+
+      // Logout user
+      var getAgent = function() {
+
+      }
+
+    /*
+      RETURN
+    */
+    return {
+      getDirectory: getDirectory,
+      getAgent: getAgent
+    }
 
   });
