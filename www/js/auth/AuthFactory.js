219dc1277f27cc6a7a43521a0f106030bd6e8b1d
Loring Dodge <loringdodge@yahoo.com>
2015-05-11 10:11:42 -0700

    Add AuthFactory.js

--- www/js/auth/AuthFactory.js
+++ www/js/auth/AuthFactory.js
@@ -2,5 +2,46 @@ angular.module('app.AuthFactory', [])
 
   .factory('AuthFactory', function($http){
 
+    /*
+      VARIABLES
+    */
+
+      var thing = {};
+
+    /*
+      HELPER FUNCTIONS
+    */
+
+      var something = function() {
+
+      }
+
+    /*
+      HTTP FUNCTIONS
+    */
+
+      // Login user
+      var login = function() {
+
+      }
+
+      // Logout user
+      var logout = function() {
+
+      }
+
+      // Signup new user
+      var signup = function() {
+
+      }
+
+    /*
+      RETURN
+    */
+    return {
+      login: login,
+      logout: logout,
+      signup: signup
+    }
 
   });
