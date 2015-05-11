2983898b07193c1cbeb62779ba2c46cd5e7e1857
Loring Dodge <loringdodge@yahoo.com>
2015-05-11 10:47:40 -0700

    Add skeleton for ChecklistFactory

--- www/js/checklist/ChecklistFactory.js
+++ www/js/checklist/ChecklistFactory.js
@@ -2,5 +2,35 @@ angular.module('app.ChecklistFactory', [])
 
   .factory('ChecklistFactory', function($http){
 
+    /*
+      VARIABLES
+    */
+
+      var checklist = {};
+
+    /*
+      FUNCTIONS
+    */
+
+      var addItem = function() {
+
+      }
+
+      var removeItem = function() {
+
+      }
+
+      var toggleCheck = function() {
+
+      }
+
+    /*
+      RETURN
+    */
+    return {
+      addItem: addItem,
+      removeItem: removeItem,
+      toggleCheck: toggleCheck
+    }
 
   });
