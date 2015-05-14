b330686b87e3d2f754873f5fa2a4a3f800c20e6a
Loring Dodge <loringdodge@yahoo.com>
2015-05-13 17:03:02 -0700

    Add deletetion to checklist items

--- www/js/checklist/ChecklistController.js
+++ www/js/checklist/ChecklistController.js
@@ -1,13 +1,6 @@
  angular.module('app.ChecklistController', [])
 
-  .controller('ChecklistController', function($scope, ChecklistFactory){
-
-    /*
-      PRIVATE VARIABLES
-    */
-
-      // Object - profile information for a single agent
-      var user = {};
+  .controller('ChecklistController', function($scope, $ionicListDelegate, ChecklistFactory){
 
     /*
       SCOPE VARIABLES
@@ -16,5 +9,19 @@
       // Array - list of checklist items
       // $scope.checklist = ChecklistFactory.checklist;
       $scope.checklist = ChecklistFactory.checklist;
+
+      $scope.showDeleteButtons = function() {
+        $ionicListDelegate.showDelete(true);
+      };
+
+      $scope.moveItem = function(item, fromIndex, toIndex) {
+        $scope.checklist.splice(fromIndex, 1);
+        $scope.checklist.splice(toIndex, 0, item);
+      };
+
+      $scope.onItemDelete = function(item) {
+        console.log(item);
+        $scope.checklist.splice($scope.checklist.indexOf(item), 1);
+      };
 
   });
