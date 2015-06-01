655faeb04924da745408e1705776889147cc12a7
Loring Dodge <loringdodge@yahoo.com>
2015-06-01 14:40:48 -0700

    Add animations to CommissionsDirective

--- www/js/request/CommissionDirective.js
+++ www/js/request/CommissionDirective.js
@@ -1,29 +1,32 @@
 angular.module('app.CommissionDirective', [])
 
-  .directive('commissionDirective', function(){
+  .directive('commissionDirective', function($timeout){
 
     var link = function(scope, element, attributes) {
+
+      var barContainer = element.children('.commission-flex');
+      var bars = barContainer.children('commission-bar');
+      var dot = element.children()[0];
 
       var randomInt = function(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
       }
 
-      var barContainer = element.children('.commission-flex');
-      var bars = barContainer.children('commission-bar');
-
       angular.forEach(bars, function(bar, index){
-        console.log(index);
-        setTimeout(function(){
+        $timeout(function(){
           angular.element(bar)
-            .addClass('commission-animate-up')
+            .addClass('commission-bar-animate-up')
             .css({
             height: randomInt(5, 80) + "%"
           });
         }, randomInt(0, bars.length) * 100 );
-          // .css({
-          //   height: randomInt(5, 80) + "%"
-          // });
-      })
+
+      });
+
+      $timeout(function(){
+        angular.element(dot)
+          .addClass('commission-dot-animate-wide')
+      }, 100);
 
     }
 
