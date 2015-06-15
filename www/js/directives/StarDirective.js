3308888a684a2fdce4eaa9952ebd06ce8621ebd0
Loring Dodge <loringdodge@yahoo.com>
2015-06-15 09:18:43 -0700

    Add comments to starDirective

--- www/js/directives/StarDirective.js
+++ www/js/directives/StarDirective.js
@@ -2,11 +2,21 @@ angular.module('app.StarDirective', [])
 
   .directive('starDirective', function($timeout){
 
+    /*
+      CONTROLLER
+    */
+
     var controller = function($scope) {
-      $scope.parseStars = function(n) {
-        return new Array(Math.round(n));
-      }
+
+        $scope.parseStars = function(n) {
+          return new Array(Math.round(n));
+        }
+
     };
+
+    /*
+      CONFIG
+    */
 
     return {
       restrict: 'E',
